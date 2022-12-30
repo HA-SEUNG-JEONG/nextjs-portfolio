3cdece1c8928688070997a708b64826cba82a9e3
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-30 21:40:18 +0900

    Style : 다크모드 마무리

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,21 +4,23 @@ import Animation from "./Animation";
 
 const Content = () => {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+    <section className="body-font flex min-h-screen flex-col items-center justify-center text-gray-600">
+      <div className="container mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
+        <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
+          <h1 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
             프론트엔드 개발자
             <br className="hidden lg:inline-block" />
             정하승입니다.
           </h1>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              <Link href="/project">프로젝트 보러가기</Link>
+            <button className="inline-flex rounded border-0 bg-indigo-500 py-2 px-6 text-lg hover:bg-indigo-600 focus:outline-none">
+              <Link className="text-black dark:text-white " href="/project">
+                프로젝트 보러가기
+              </Link>
             </button>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
           <Animation />
         </div>
       </div>
