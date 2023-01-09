4f323e5bc435a1b750dacb2a8e2ad1ced8e992dc
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-09 16:59:30 +0900

    Feat : 깃허브,배포 링크 추가

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 import { Page } from "../../pages/project";
 
 const ProjectItem = ({ data }: { data: Page }) => {
-  const projectTitle = data.properties["이름"].title[0].plain_text;
-  const githubLink = data.properties.URL.url;
-  const description = data.properties.Description.rich_text[0].plain_text;
-  const tags = data.properties["태그"].multi_select;
+  const projectTitle = data.properties.name.title[0].plain_text;
+  const deployLink = data.properties.url.url;
+  const description = data.properties.description.rich_text[0].plain_text;
+  const tags = data.properties.tag.multi_select;
   const imgsrc = data.cover?.file?.url || data.cover.external.url;
+  const githubLink = data.properties.githubLink.rich_text[0].text.link.url;
 
   return (
-    <div className="project-card">
+    <article className="project-card">
       <section className="w-full">
         <Image
           className="rounded-t-xl"
@@ -24,19 +25,22 @@ const ProjectItem = ({ data }: { data: Page }) => {
         />
       </section>
 
-      <div className="flex w-full flex-col p-4">
+      <section className="flex w-full flex-col p-4">
         <h1 className="text-xl font-bold">{projectTitle}</h1>
-        <h3 className="text-md mt-3">{description}</h3>
-        <Link href={githubLink}>깃허브 링크</Link>
-        <div className="mt-2 flex items-start space-x-2">
+        <section className="flex flex-col space-y-2">
+          <h3 className="text-md mt-3">{description}</h3>
+          <Link href={deployLink}>배포 링크</Link>
+          <Link href={githubLink}>깃허브 링크</Link>
+        </section>
+        <section className="mt-2 flex items-start space-x-2">
           {tags.map((tag: any) => (
             <h1 className="w-30 rounded-md bg-sky-200 px-2 py-1 dark:bg-sky-700 " key={tag.id}>
               {tag.name}
             </h1>
           ))}
-        </div>
-      </div>
-    </div>
+        </section>
+      </section>
+    </article>
   );
 };
 
