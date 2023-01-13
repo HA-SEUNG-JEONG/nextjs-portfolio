517cbe2b7fe6a748bb3d7bf4099cf079d1bfbce9
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-13 22:56:36 +0900

    Style : 스킬 페이지 기본 배치

--- pages/skill.tsx
+++ pages/skill.tsx
@@ -2,9 +2,70 @@ import React from "react";
 
 const skill = () => {
   return (
-    <div>
-      <span className="text-xl">JavaScript</span>
-      <span className="text-xl">TypeScript</span>
+    <div className="grid grid-cols-4 gap-5 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">HTML</div>
+          <p className="text-base text-gray-700 ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">CSS</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">Forest</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">Forest</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">Forest</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">Forest</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded shadow-lg">
+        <div className="bg-slate-800 px-6 py-4">
+          <div className="mb-2 text-xl font-bold">Forest</div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea!
+            Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
