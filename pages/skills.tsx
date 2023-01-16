ef7779047eb4422d8949fa66a49f49c1c42aa907
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-16 18:36:44 +0900

    Chore : skills로 변경

---
+++ pages/skills.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const skill = () => {
+  return (
+    <>
+      <Header />
+      <div className="grid grid-cols-4 gap-5 bg-slate-800 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">HTML</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                웹 접근성 및 웹 표준을 위해 시맨틱 태그를 이용하여 프로젝트에 활용이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">CSS</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">flex를 활용한 컨텐츠 배치가 가능합니다.</li>
+              <li className="text-base text-gray-300">
+                적절한 페이지에 반응형 디자인 적용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                UI/UX 관점에서 어떻게 하면 사용자에게 더 좋은 서비스를 제공할지 고민하고 있습니다.
+              </li>
+              <li className="text-base text-gray-300">
+                사용 가능한 CSS 프레임워크 및 라이브러리 : Tailwind CSS, styled-components
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-60 overflow-hidden rounded bg-slate-700 shadow-xl">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">JavaScript</div>
+            <ul className="space-y-2">
+              <li className="text-base text-gray-300">
+                ES2015(ES6) 문법을 이해하고 알고리즘 및 프로젝트에 활용이 가능합니다.
+              </li>
+              <li className="text-base text-gray-300">
+                HTTP를 이해하고, axios를 활용하여 REST API 연동 작업이 가능합니다.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default skill;
