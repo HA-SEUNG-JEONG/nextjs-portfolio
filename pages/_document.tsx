41edab9de61a90afe95ca45a8f2f8c9f0f085d33
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-16 20:01:00 +0900

    Chore : Head 태그 안에 메타 태그 삽입

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <meta name="robots" content="all" />
-      <Head />
+      <Head>
+        <meta name="robots" content="all" />
+      </Head>
       <body>
         <Main />
         <NextScript />
