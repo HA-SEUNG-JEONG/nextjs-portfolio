2f8c10ad2e4f438abae20f267fdee6d131127c43
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-16 19:46:26 +0900

    Style : min-height 조정

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,7 +4,7 @@ import Animation from "./Animation";
 
 const Content = () => {
   return (
-    <section className="body-font flex min-h-screen flex-col items-center justify-center text-gray-600">
+    <section className="flex min-h-[80vh] flex-col items-center justify-center overflow-y-hidden text-gray-600">
       <div className="container mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
         <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
           <h1 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
