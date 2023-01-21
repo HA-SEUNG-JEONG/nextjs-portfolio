4806707c4e86ec42a17c42b0e8962d55fb75eb39
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-21 14:21:43 +0900

    Chore : 소개 문구 수정

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import Animation from "./Animation";
 
 const Content = () => {
   return (
-    <section className="flex min-h-[80vh] flex-col items-center justify-center overflow-y-hidden text-gray-600">
+    <section className="flex min-h-[849px] flex-col items-center justify-center overflow-y-hidden text-gray-600">
       <div className="container mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
         <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
           <h1 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
+            유저 경험을 중시하는
+            <br className="hidden lg:inline-block" />
             프론트엔드 개발자
             <br className="hidden lg:inline-block" />
             정하승입니다.
@@ -20,8 +21,15 @@ const Content = () => {
             </button>
           </div>
         </div>
-        <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
-          <Animation />
+        <div className="md:w-80 lg:w-96 lg:max-w-lg">
+          <Image
+            priority
+            width={500}
+            height={500}
+            src="/../public/profile.jpg"
+            alt="profile"
+            quality={100}
+          />
         </div>
       </div>
     </section>
