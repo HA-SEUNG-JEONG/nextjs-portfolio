457bd6bd4673b2ed1cc2350cbbef31a157cde92d
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-21 14:42:44 +0900

    Feat : @next/font 적용 - 구글 Roboto 사용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["vietnamese"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </ThemeProvider>
   );
 }
